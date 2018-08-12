8decf2648307d3584b6a7131bbc70024843f8b40
Michael Hodges <mike.everywhere@gmail.com>
2018-08-11 22:34:34 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,103 @@
+/*TODO:
+ * 
+ * 
+*/
+
+//canvas creation
+var canvas = document.getElementById("imgCanvas");
+var context = canvas.getContext("2d");
+
+//game tick increases constantly
+var tick = 0;
+
+function Point(x, y, velX, velY, lines, health = 5, stuck = false) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.health = health;
+    this.stuck = stuck;//whether the point is stuck to a wall
+    this.lines = lines;//array of lines
+}
+
+function Line(x, y, x2, y2, health = 1, stuck = false) {
+    this.x = x;
+    this.y = y;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.health = health;
+    this.stuck = stuck;//whether the point is stuck to a wall
+}
+
+//list of all points
+var pointList = [];
+
+function pointUpdate() {
+    pointList.forEach(element => {
+        if(!element.stuck) {
+            element.x += element.velX;
+            element.y += element.velY;
+
+            if(element.y <= 0) {
+                element.y = 0;
+                element.stuck = true;
+            }
+            else if(element.y >= 248) {
+                element.y = 248;
+                element.stuck = true;
+            }
+            else if(element.x >= 1016) {
+                element.x = 1016;
+                element.stuck = true;
+            }
+            //place projectile collision here
+        }
+        
+    });
+}
+
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#4800FF";
+    
+
+    pointList.forEach(element => {
+        element.lines.forEach(element2 => {
+            
+        });
+        context.fillRect(element.x, element.y, 8, 8);
+    });
+}
+
+function update() {
+    if(tick % 60 == 0) {
+        var tempLines = [];
+        pointList.forEach(element => {
+            tempLines.push(new Line(0, 128, element.x, element.y));
+        });
+        pointList.push(new Point(0, 128, Math.random(), (Math.random() * 2) - 1), tempLines);//temp testing values
+    }
+
+    pointUpdate();
+}
+
+function mainLoop() {
+
+    document.getElementById("TICKS").innerHTML = "Ticks: " + tick;
+    tick++;
+
+    update();
+    render();
+    requestAnimationFrame(mainLoop);
+}
+
+requestAnimationFrame(mainLoop);
+
+//not actually used yet
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
+}
